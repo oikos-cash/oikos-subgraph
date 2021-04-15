0e136460a0456fce481fbe50273918b7c4e3aae1
Oikos.cash <albert@oikos.cash>
2021-04-15 10:01:44 -0400

    fix mappings

--- src/common.ts
+++ src/common.ts
@@ -1,16 +1,17 @@
 import { Bytes, ByteArray, BigInt } from '@graphprotocol/graph-ts';
 
 import { Oikos as OKS } from '../generated/Oikos/Oikos';
+import { ExchangeRates } from '../generated/ExchangeRates/ExchangeRates';
 
 export let sUSD32 = ByteArray.fromHexString(
-  '0x7355534400000000000000000000000000000000000000000000000000000000',
+  '0x6f55534400000000000000000000000000000000000000000000000000000000',
 ) as Bytes;
 export let sUSD4 = ByteArray.fromHexString('0x73555344') as Bytes;
 
-export function attemptEffectiveValue(oikos: OKS, currencyKey: Bytes, amount: BigInt): BigInt {
+export function attemptEffectiveValue(exRates: ExchangeRates, currencyKey: Bytes, amount: BigInt): BigInt {
   let sUSD = sUSD32;
 
-  let effectiveValueTry = oikos.try_effectiveValue(currencyKey, amount, sUSD);
+  let effectiveValueTry = exRates.try_effectiveValue(currencyKey, amount, sUSD);
   if (!effectiveValueTry.reverted) {
     return effectiveValueTry.value;
   }
