6be96ef6bf22ff1b512e102393aaca6ffd06dd61
Oikos.cash <albert@oikos.cash>
2021-07-09 11:13:42 -0400

    adapt for mustache

---
+++ mustache/templates/exchanger/contracts.js
@@ -0,0 +1,9 @@
+module.exports = [
+  {
+    prod: 8960693,
+    test: null,
+    type: 'exchanger',
+    name: 'Exchanger',
+    address: "'0xf283B16be2C742AdC682facB5a06609De05A032D'",
+  }
+];
---
+++ mustache/templates/exchanger/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  description: 'Oikos Exchanger API',
+  schemaFile: './oikos-exchanger.graphql',
+};
